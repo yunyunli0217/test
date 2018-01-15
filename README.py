31f7830aca80f5a45d7d1d39d50a24bbebd86c38
yunyunli0217 <33297689+yunyunli0217@users.noreply.github.com>
2018-01-14 20:57:15 -0500

    Update README.py

--- README.py
+++ README.py
@@ -3,3 +3,8 @@
 import pandas as pd
 c=pd.read_csv("CLIENT_REVENUE2.csv")
 w = pd.read_csv("VW_MANAGED2.csv",error_bad_lines=False)
+w.drop("party_survey_response_id"  ,axis=1,inplace=True)
+
+df=[w,c]
+df=pd.concat(df)
+df.shape
